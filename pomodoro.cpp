8c08cfe9285687352a66329017126a1d4614d736
feliperapacci <felprapacci@gmail.com>
2026-01-22 14:48:49 -0500

    Countdown is properly formatted to h, m, s

--- pomodoro.cpp
+++ pomodoro.cpp
@@ -1,22 +1,60 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <iomanip>
 
-void printTime();
+const int INTERVAL_TIME = 1;
+
+template <class Rep, class Period>
+void printTime(const std::chrono::duration<Rep, Period> &duration);
+// std::chrono::seconds getTime();
+
 
 int main() {
-    auto timer = std::chrono::steady_clock::now();  // initialize timer 
-    auto increment = std::chrono::seconds(1);       // will print time each second
+    
+    // now & end are of type std::chrono::time_point<std::chrono::steady_clock>
+    auto now = std::chrono::steady_clock::now();
+    auto end = now + std::chrono::seconds{ 10 };
 
-    std::cout << "\033[2J";
-    while (true) {
-        std::this_thread::sleep_until(timer += increment);
-        printTime();
+    // interval & time_remaining are of type std::chrono::duration<...>
+    auto interval = std::chrono::seconds{ INTERVAL_TIME };
+    auto time_remaining = end - now;
+
+
+    std::cout << "\033[2J"; // clears screen
+
+    
+    while (time_remaining.count() >= 1){
+        std::this_thread::sleep_until(now += interval);
+        time_remaining = end - now;
+        printTime(time_remaining);
     }    
+
+    std::cout << "Done!\n";
 }
 
-void printTime() {
-    static int x = 0;
-    std::cout << "\033[H";                          // moves cursor to beginning
-    std::cout << x++ << std::endl;                  // std::endl to flush
+template <class Rep, class Period>
+void printTime(const std::chrono::duration<Rep, Period> &duration) {
+    std::cout << "\033[H";                                  // moves cursor to beginning
+    
+#if __cplusplus >= 202002L                                  // this code for C++20 or later
+    std::chrono::hh_mm_ss time { duration };
+    std::cout << "C++20+\n";
+    auto h = time.hours();
+    auto m = time.minutes();
+    auto s = time.seconds();
+
+#else                                                       // this code for C++17 or previous
+    std::cout << "C++17-\n";
+    auto h = std::chrono::duration_cast<std::chrono::hours>(duration);
+    auto m = std::chrono::duration_cast<std::chrono::minutes>(duration - h);
+    auto s = std::chrono::duration_cast<std::chrono::seconds>(duration - h - m);
+    
+#endif
+    std::cout << h.count() << ":" << m.count() << ":" << s.count() << std::endl;
 }
+
+/*std::chrono::duration getTime() {
+    int minutes, seconds;
+
+}*/
